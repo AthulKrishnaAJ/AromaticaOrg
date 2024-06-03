bf6ab8e3c1ded336e4df3cf0ab026097c40b43ec
AthulKrishna <athulkrishna9633141960@gmail.com>
2024-06-03 20:16:07 +0530

    edit passport config

--- helpers/passport.js
+++ helpers/passport.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://aromatica.athul.cloud/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
    
         return done(null, profile);
